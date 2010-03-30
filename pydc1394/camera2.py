a9a2690cd1baa06470bd410adfd7ccbda57c34cc
Robert Jordens <jordens@debian.org>
2010-03-30 19:51:02 +0200

    make camera2.py LGPL, as the rest

--- pydc1394/camera2.py
+++ pydc1394/camera2.py
@@ -4,18 +4,18 @@
 #
 # This file is part of pydc1394.
 #
-# pydc1394 is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+#   Pydc1394 is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# pydc1394 is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#   Pydc1394 is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with pydc1394.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#   along with Pydc1394.  If not, see <http://www.gnu.org/licenses/>.
 
 from _dc1394core import *
 from _dc1394core import _dll
