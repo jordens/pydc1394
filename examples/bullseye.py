ec0a6d0e5c9fb57c315d6ab72a63387844bde3ba
jordens@phys.ethz.ch <>
2012-02-18 02:09:09 -0700

    bullseye.py: roi yet better

--- examples/bullseye.py
+++ examples/bullseye.py
@@ -11,9 +11,8 @@ if ETSConfig.toolkit == "wx":
     import wx
     constants.WindowColor = wx.NullColor
 
-
 from traits.api import (HasTraits, Range, Float, Enum,
-        on_trait_change, TraitError, TraitType, 
+        on_trait_change, TraitError, Event, ListFloat,
         Instance, Bool, Int, Button)
 
 from traitsui.api import (View, Item, UItem,
@@ -40,42 +39,24 @@ from scipy import stats, optimize
 from threading import Thread
 
 
-class RoiTrait(TraitType):
-    def validate(self, obj, name, value):
-        try:
-            ac = obj.active
-            obj.active = False
-            x, y, w, h = value
-            x = min(1280, max(0, x))
-            y = min(960, max(0, y))
-            w = min(1280-x, max(8, w))
-            h = min(960-y, max(2, h))
-            y = 960-h-y
-            (w, h), (x, y), _, _ = obj.cam.mode.setup(
-                    (w, h), (x, y), "Y8")
-            obj.active = ac
-            y = 960-h-y
-            return x, y, w, h
-        except Exception, e:
-            self.error(obj, name, value)
-
 class Camera(HasTraits):
     cam = Instance(DC1394Camera)
 
-    shutter = Range(1e-5, 100e-3, 1e-3)
+    shutter = Range(5e-6, 100e-3, 1e-3)
     gain = Range(0., 24., 0.)
     framerate = Range(1, 10, 2)
     average = Range(1, 10, 1)
 
     auto_shutter = Button("Auto")
+    auto_shutter_requested = Bool(False)
 
     pixelsize = Float(3.75)
 
     thread = Instance(Thread)
     active = Bool(False)
 
-    roi = RoiTrait((0, 0, 1280, 960))
-    #roi = ListInt([0, 0, 1280, 960], minlen=4, maxlen=4)
+    #roi = RoiTrait((0, 0, 1280, 960))
+    roi = ListFloat([0, 0, 1280, 960], minlen=4, maxlen=4)
 
     background = Bool(False)
 
@@ -85,6 +66,7 @@ class Camera(HasTraits):
     e = Float
     a = Float
     b = Float
+    d = Float
 
     def __init__(self, uri, **k):
         super(Camera, self).__init__(**k)
@@ -95,15 +77,21 @@ class Camera(HasTraits):
             self.cam = DC1394Camera()
         elif scheme == "none":
             self.cam = None
-        self.im = 0
+        self.im = None
+        self.grid = None
         if self.cam:
             self.setup()
+
+    def initialize(self):
+        self.update_roi()
+        self.start()
+        self.capture()
+        self.process()
+        self.stop()
 
     def setup(self):
         self.mode = self.cam.modes_dict["FORMAT7_0"]
         self.cam.mode = self.mode
-        self.roi = self.roi
-        self.bounds = None
         self.cam.setup(framerate=self.framerate,
                 gain=self.gain, shutter=self.shutter)
         self.cam.setup(active=False,
@@ -135,62 +123,80 @@ class Camera(HasTraits):
 
     @on_trait_change("auto_shutter")
     def _do_auto_shutter(self):
-        ac = self.active
+        self.auto_shutter_requested = True
+
+    def auto(self):
         fr = self.cam.framerate.absolute
-        self.active = False
         self.cam.framerate.absolute = max(
                 self.cam.framerate.absolute_range)
-        self.start()
         for i in range(20):
             im_ = self.cam.dequeue()
-            im = np.array(im_).copy()
+            im = np.array(im_).astype("float")/(1<<16)
             im_.enqueue()
             # undo gamma
             p = np.percentile(im, 99)
-            print "1%%>%g," % p,
+            
             try:
-                if p > .75*255:
-                    self.shutter *= .8
-                    print "t-: %g" % self.shutter
-                elif p < .25*255:
-                    self.shutter /= .8
-                    print "t+: %g" % self.shutter
+                if p > .75:
+                    self.shutter *= .6
+                    s = "-"
+                elif p < .25:
+                    self.shutter /= .6
+                    s = "+"
                 else:
-                    print "t=: %g" % self.shutter
-                    break
+                    s = "="
             except TraitError:
                 break
             # ensure all frames with old settings are gone
             self.cam.flush()
             self.cam.dequeue().enqueue()
-        self.stop()
+            logging.debug("1%%>%g, t%s: %g" % (p, s, self.shutter))
+            if s == "=":
+                break
         # revert framerate and active state
         self.cam.framerate.absolute = fr
-        self.active = ac
+
+    def update_roi(self):
+        x, y, w, h = self.roi
+        x = min(1280, max(0, x))
+        y = min(960, max(0, y))
+        w = min(1280-x, max(128, w))
+        h = min(960-y, max(128, h))
+        y = 960-h-y
+        (w, h), (x, y), _, _ = self.mode.setup(
+                (w, h), (x, y), "Y16")
+        y = 960-h-y
+        self.bounds = x, y, w, h
+        logging.debug("%s %s" % (self.bounds, self.mode.roi))
+
+    def get_dummy():
+        px = self.pixelsize
+        l, b, w, h = self.roi
+        y, x = np.mgrid[b:b+h, l:l+w]
+        x *= px
+        y *= px
+        x -= 1.1e3
+        y -= 1.2e3
+        t = 15./180.*np.pi
+        b = 150/4.
+        a = 250/4.
+        h = 200
+        x, y = np.cos(t)*x+np.sin(t)*y, -np.sin(t)*x+np.cos(t)*y
+        im = h*np.exp(-x**2/a**2/2.-y**2/b**2/2.)
+        im *= 1+np.random.randn(*im.shape)*.2
+        #im += np.random.randn(im.shape)*30
+        return im
 
     def capture(self):
         if self.cam:
             im_ = self.cam.dequeue()
-            im = np.array(im_)/255.
+            im = np.array(im_).astype("float")/(1<<16)
             im_.enqueue()
             # undo gamma
-            print im.shape, self.roi, self.cam.mode.roi, im.ptp()
+            logging.debug("%s %s %s %s" % (
+                im.shape, self.roi, self.cam.mode.roi, im.ptp()))
         else:
-            px = self.pixelsize
-            l, b, w, h = self.roi
-            y, x = np.mgrid[b:b+h, l:l+w]
-            x *= px
-            y *= px
-            x -= 1.1e3
-            y -= 1.2e3
-            t = 15./180.*np.pi
-            b = 150/4.
-            a = 250/4.
-            h = 200
-            x, y = np.cos(t)*x+np.sin(t)*y, -np.sin(t)*x+np.cos(t)*y
-            im = h*np.exp(-x**2/a**2/2.-y**2/b**2/2.)
-            im *= 1+np.random.randn(*im.shape)*.2
-            #im += np.random.randn(im.shape)*30
+            im = self.get_dummy()
         if self.average > 1 and self.im.shape == im.shape:
             self.im = self.im*(1-1./self.average) + im/self.average
         else:
@@ -198,10 +204,9 @@ class Camera(HasTraits):
 
     def process(self):
         im = self.im
-        if self.roi != self.bounds:
-            self.update_bounds()
-
         px = self.pixelsize
+        l, b, w, h = self.bounds
+
         if self.background:
             #imr = im.ravel()
             #low = stats.scoreatpercentile(imr, 20)
@@ -210,18 +215,18 @@ class Camera(HasTraits):
             #im -= bg_mean
             im -= np.percentile(im, 5)
 
-        self.data.set_data("img", im)
-        self.data.set_data("imx", im.sum(axis=0))
-        self.data.set_data("imy", im.sum(axis=1))
+        y, x = np.ogrid[b:b+h, l:l+w]
+        xbounds = (np.r_[x[0, :], (l+w)]-.5)*px
+        ybounds = (np.r_[y[:, 0], (b+h)]-.5)*px
+        #xbounds = (x[0, 0]-.5)*px, (x[0, -1]+.5)*px
+        #ybounds = (y[0, 0]-.5)*px, (y[-1, 0]+.5)*px
 
-        x, y = self.pxx, self.pxy
         m00 = im.sum() or 1.
-        im = im/m00
-        m10, m01 = (im*x).sum(), (im*y).sum()
-        x -= m10
-        y -= m01
-        m20, m02 = (im*x**2).sum(), (im*y**2).sum()
-        m11 = (im*x*y).sum()
+        imn = im/m00
+        m10, m01 = (imn*x).sum(), (imn*y).sum()
+        dx, dy = x-m10, y-m01
+        m20, m02 = (imn*dx**2).sum(), (imn*dy**2).sum()
+        m11 = (imn*dx*dy).sum()
 
         g = np.sign(m20-m02)
         if g == 0:
@@ -241,6 +246,7 @@ class Camera(HasTraits):
         self.t = t/np.pi*180
         self.a = a*px
         self.b = b*px
+        self.d = ab*px
         self.e = e
 
         # http://www.ipol.im/pub/algo/g_linear_methods_for_image_interpolation
@@ -254,14 +260,35 @@ class Camera(HasTraits):
         # (4950.0, 4999.5005)
         imr = np.array(Image.fromarray(im).rotate(angle=np.rad2deg(t),
             resample=Image.NEAREST, expand=True))
-       
-        self.data.set_data("ima", imr.sum(axis=0))
-        self.data.set_data("imb", imr.sum(axis=1))
         b, a = np.ogrid[:imr.shape[0], :imr.shape[1]]
 
-        self.data.set_data("a", a[0, :]*px)
-        self.data.set_data("b", b[:, 0]*px)
-        
+        upd = dict((
+            ("img", im),
+            ("imx", im.sum(axis=0)),
+            ("imy", im.sum(axis=1)),
+            ("ima", imr.sum(axis=0)),
+            ("imb", imr.sum(axis=1)),
+            ("a", a[0, :]*px),
+            ("b", b[:, 0]*px),
+            ("x", x[0, :]*px),
+            ("y", y[:, 0]*px),
+            ("xbounds", xbounds),
+            ("ybounds", ybounds),
+            ))
+        self.data.arrays.update(upd)
+        self.data.data_changed = {"changed": upd.keys()}
+        if self.grid:
+            self.grid.set_data(xbounds, ybounds)
+            #enforce data/screen aspect ratio 1
+            sl, sr, sb, st = self.gridm.screen_bounds
+            dl, db = self.gridm.range.low
+            dr, dt = self.gridm.range.high
+            dsdx = float(sr-sl)/(dr-dl)
+            dt_new = db+(st-sb)/dsdx
+            #dsdy = float(st-sb)/(dt-db)
+            #print dsdx, dsdy, dt, dt_new
+            self.gridm.range.y_range.high_setting = dt_new
+       
         return
 
         gx = m00*self.camera.pixelsize/(2*np.pi*m20)**.5*np.exp(-x[:, 0]**2/m20/2)
@@ -294,24 +321,15 @@ class Camera(HasTraits):
         self.data.set_data("ym_mark", 2*[min(ell2_y)])
         self.data.set_data("y_bar", [0, max(self.data.get_data("imy"))])
 
-    def update_bounds(self):
-        px = self.pixelsize
-        l, b, w, h = self.roi
-        self.pxy, self.pxx = np.ogrid[b:b+h, l:l+w]
-        self.data.set_data("x", self.pxx[0, :]*px)
-        self.data.set_data("y", self.pxy[:, 0]*px)
-        xbounds = (np.r_[self.pxx[0, :], (l+w)]-.5)*px
-        ybounds = (np.r_[self.pxy[:, 0], (b+h)]-.5)*px
-        self.data.set_data("xbounds", xbounds)
-        self.data.set_data("ybounds", ybounds)
-        self.bounds = l, b, w, h
-
     @on_trait_change("active")
     def _start_me(self, value):
         if value:
-            print "starting capture"
             if self.thread is not None:
-                print "already have a capture thread, try again"
+                if not self.thread.is_alive():
+                    self.thread.join()
+                    self.thread = None
+                else:
+                    logging.warning("already have a capture thread, try again")
                 return
             else:
                 self.thread = Thread(target=self.run)
@@ -322,17 +340,20 @@ class Camera(HasTraits):
                 assert self.thread is None
 
     def run(self):
-        print "start"
+        self.update_roi()
+        logging.debug("start")
         self.start()
         while self.active:
+            if self.auto_shutter_requested:
+                self.auto()
+                self.auto_shutter_requested = False
             self.capture()
-            print "captured"
+            logging.debug("captured")
             self.process()
-            print "processed"
-        print "stop"
+            logging.debug("processed")
+        logging.debug("stop")
         self.stop()
         self.thread = None
-
 
 slider_editor=DefaultOverride(mode="slider")
 
@@ -355,8 +376,9 @@ class Bullseye(HasTraits):
                 Item("object.camera.x", label="Centroid X", format_str="%g"),
                 Item("object.camera.y", label="Centroid Y", format_str="%g"),
                 Item("object.camera.t", label="Angle", format_str="%g"),
-                Item("object.camera.a", label="Major axis (4w)", format_str="%g"),
-                Item("object.camera.b", label="Minor axis (4w)", format_str="%g"),
+                Item("object.camera.a", label="Major (4w)", format_str="%g"),
+                Item("object.camera.b", label="Minor (4w)", format_str="%g"),
+                Item("object.camera.d", label="Mean (4w)", format_str="%g"),
                 Item("object.camera.e", label="Ellipticity", format_str="%g"),
                 style="readonly"),
             VGroup(HGroup(
@@ -371,11 +393,12 @@ class Bullseye(HasTraits):
                 "palette"),
             ),
         VGroup(
-            UItem("plots", editor=ComponentEditor()),
+            UItem("plots", editor=ComponentEditor(),
+                width=(1280+105)/2, height=(960+105)/2),
             UItem("abplots", editor=ComponentEditor(),
                 height=-200, resizable=False),
             ),
-        ), resizable=True, title='Bullseye')
+        ), resizable=True, title="Bullseye")
 
     def __init__(self, uri="first:", **k):
         super(Bullseye, self).__init__(**k)
@@ -383,10 +406,7 @@ class Bullseye(HasTraits):
 
         self.camera = Camera(uri)
         self.camera.data = self.data
-        self.camera.start()
-        self.camera.capture()
-        self.camera.process()
-        self.camera.stop()
+        self.camera.initialize()
 
         self.plots = GridPlotContainer(shape=(2,2), padding=0,
                 use_backbuffer=True, fill_padding=True,
@@ -416,6 +436,7 @@ class Bullseye(HasTraits):
         self.horiz.index_grid.visible = True
         self.horiz.value_grid.visible = False
         self.horiz.value_mapper.range.low_setting = 0
+        self.horiz.index_range = self.screen.index_range
         self.vert = Plot(self.data,
                 orientation="v",
                 resizable="v", padding=0, width=100,
@@ -425,7 +446,8 @@ class Bullseye(HasTraits):
         self.vert.value_axis.visible = False
         self.vert.index_grid.visible = True
         self.vert.value_grid.visible = False
-        self.vert.value_mapper.range.low_setting = 0
+        self.vert.value_range = self.horiz.value_range
+        self.vert.index_range = self.screen.value_range
 
         self.mini = VPlotContainer(
                 width=100, height=100, resizable="",
@@ -434,10 +456,6 @@ class Bullseye(HasTraits):
         self.plots.component_grid = [
                 [self.vert, self.screen],
                 [self.mini, self.horiz]]
-        #self.screen.index_range = self.horiz.index_range
-        #self.screen.value_range = self.vert.index_range
-        self.horiz.index_range = self.screen.index_range
-        self.vert.index_range = self.screen.value_range
 
         self.screen.overlays.append(ZoomTool(self.screen,
             tool_mode="box", alpha=.3,
@@ -452,19 +470,23 @@ class Bullseye(HasTraits):
         self.plots.tools.append(SaveTool(self.plots,
             filename="bullseye.pdf"))
 
-        i = self.screen.img_plot("img", name="img",
-                xbounds="xbounds", ybounds="ybounds",
-                interpolation="nearest",
-                colormap=color_map_name_dict[self.palette],
-                )[0]
-        i.color_mapper.range.low_setting = 0
-        i.color_mapper.range.high_setting = 255
-        t = ImageInspectorTool(i)
-        self.screen.tools.append(t)
-        i.overlays.append(ImageInspectorOverlay(
-            component=i, image_inspector=t,
-            border_size=0, bgcolor="darkgray", align="ur",
-            tooltip_mode=False, font="modern 10"))
+        if True:
+            self.screenplot = self.screen.img_plot("img", name="img",
+                    xbounds="xbounds", ybounds="ybounds",
+                    interpolation="nearest",
+                    colormap=color_map_name_dict[self.palette],
+                    )[0]
+            #self.screen.aspect_ratio = 1280/960.
+            self.screenplot.color_mapper.range.low_setting = 0
+            self.screenplot.color_mapper.range.high_setting = 1
+            self.camera.grid = self.screenplot.index
+            self.camera.gridm = self.screenplot.index_mapper
+            t = ImageInspectorTool(self.screenplot)
+            self.screen.tools.append(t)
+            self.screenplot.overlays.append(ImageInspectorOverlay(
+                component=self.screenplot, image_inspector=t,
+                border_size=0, bgcolor="darkgray", align="ur",
+                tooltip_mode=False, font="modern 10"))
 
         self.horiz.plot(("x", "imx"), type="line", color="red")
         #self.horiz.plot(("x", "gauss_x"), type="line", color="blue")
@@ -482,7 +504,6 @@ class Bullseye(HasTraits):
         self.asum.value_axis.visible = False
         self.asum.index_axis.tick_label_color = "white"
         self.asum.index_axis.axis_line_color = "white"
-        self.asum.value_mapper.range.low_setting = 0
         self.asum.value_grid.visible = False
         self.bsum = Plot(self.data,
                 padding=0,
@@ -492,12 +513,12 @@ class Bullseye(HasTraits):
         self.bsum.value_axis.visible = False
         self.bsum.index_axis.tick_label_color = "white"
         self.bsum.index_axis.axis_line_color = "white"
-        self.bsum.value_mapper.range.low_setting = 0
         self.bsum.value_grid.visible = False
+        self.bsum.value_range = self.asum.value_range
+        self.bsum.index_range = self.asum.index_range
 
         self.abplots.add(self.asum)
         self.abplots.add(self.bsum)
-        self.bsum.value_range = self.asum.value_range
         self.asum.plot(("a", "ima"), type="line", color="red")
         self.bsum.plot(("b", "imb"), type="line", color="red")
 
@@ -541,10 +562,11 @@ class Bullseye(HasTraits):
         l, r = self.screen.index_range.low, self.screen.index_range.high
         b, t = self.screen.value_range.low, self.screen.value_range.high
         px = self.camera.pixelsize
-        self.camera.roi = l/px, b/px, (r-l)/px, (t-b)/px
+        self.camera.roi = [l/px, b/px, (r-l)/px, (t-b)/px]
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s')
     b = Bullseye("first:")
     #b = Bullseye("none:")
     #b = Bullseye("guid:b09d01009981f9")
